e9b9c3343c77dcf07a6e716a7250001e5d005468
Evelin Cobos <evecobos8@gmail.com>
2026-02-16 20:06:44 -0500

    done

--- project/src/fetch-helpers.js
+++ project/src/fetch-helpers.js
@@ -6,6 +6,56 @@ const randomize = () => {
     return randomNum
 }
 
-const getPokemon = async () => {
-    
+export const getRandomPokemon = async () => { 
+    const pokemonObj = {}
+    try {
+    const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomize()}`);
+    if (!promise.ok) {
+        throw Error(`Fetch failed. ${promise.status} ${promise.statusText}`);
+    }
+    const responseData = await promise.json()
+    //console.log(responseData)
+
+    pokemonObj.name = responseData.name
+
+    responseData.types.forEach(obj => {{
+        pokemonObj.types = obj.type.name
+    }}
+    )
+
+    pokemonObj.sprite = responseData.sprites.back_default
+
+    //console.log(pokemonObj)
+
+    return { data: pokemonObj, error: null };
 }
+    catch (error) {
+        console.log(`Error: ${error.message}`);
+        return{ data: null, error: error };
+    }
+}
+
+export const postDiscoveredPokemon = async (formData) => { //request to upload data from form to formspree
+    try {
+        const promise = await fetch('https://formspree.io/f/mkovbbpy', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'content-type': 'application/json', // <-- the data format we're sending
+                'accept': 'application/json'        // <-- the data format we can receive
+            }
+        }) 
+        if (!promise.ok) {
+            throw Error(`Failed to submit. ${promise.status} ${promise.statusText}`);
+        }
+        const responseData = await promise.json();
+        console.log(responseData) 
+
+        return { data: responseData, error: null};
+    }
+    catch (error) {
+        console.log(`Error: ${error.message}`);
+        return { data: null, error: error };
+    }
+}
+
